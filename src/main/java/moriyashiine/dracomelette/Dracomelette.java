70983a0efcf580ade85bfb2c38c9d52107edfbc9
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2020-05-16 12:37:08 -0500

    Update Dracomelette.java

--- src/main/java/moriyashiine/dracomelette/Dracomelette.java
+++ src/main/java/moriyashiine/dracomelette/Dracomelette.java
@@ -2,7 +2,6 @@ package moriyashiine.dracomelette;
 
 import net.minecraftforge.fml.common.Mod;
 
-/** File created by mason on 4/18/20 **/
 @Mod(Dracomelette.MODID)
 public class Dracomelette {
 	public static final String MODID = "dracomelette";
