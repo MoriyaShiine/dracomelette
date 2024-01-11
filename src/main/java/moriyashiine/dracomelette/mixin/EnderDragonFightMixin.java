117f000a1664610d1081413db97f0333079036bd
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-01-11 10:09:10 -0600

    change logic so yung's better end island doesn't completely override the dragon killed method

--- src/main/java/moriyashiine/dracomelette/mixin/EnderDragonFightMixin.java
+++ src/main/java/moriyashiine/dracomelette/mixin/EnderDragonFightMixin.java
@@ -7,20 +7,27 @@ package moriyashiine.dracomelette.mixin;
 import moriyashiine.dracomelette.common.ModConfig;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonFight;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(EnderDragonFight.class)
+import java.util.UUID;
+
+@Mixin(value = EnderDragonFight.class, priority = 499)
 public class EnderDragonFightMixin {
+	@Shadow
+	@Nullable
+	private UUID dragonUuid;
+
 	@Shadow
 	private boolean previouslyKilled;
 
-	@Inject(method = "dragonKilled", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/boss/dragon/EnderDragonFight;generateNewEndGateway()V"))
+	@Inject(method = "dragonKilled", at = @At("HEAD"))
 	private void dracomelette$respawnDragonEgg(EnderDragonEntity dragon, CallbackInfo ci) {
-		if (ModConfig.spawnMultipleEggs) {
+		if (ModConfig.spawnMultipleEggs && dragon.getUuid().equals(dragonUuid)) {
 			previouslyKilled = false;
 		}
 	}
